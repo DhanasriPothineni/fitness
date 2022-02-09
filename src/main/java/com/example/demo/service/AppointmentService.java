a7e260f179f8a4dc474c3a98e1706a56ef418401
Dhanasri <dhanasreepothineni@gmail.com>
2022-02-09 13:54:53 +0530

    First commit

---
+++ src/main/java/com/example/demo/service/AppointmentService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Appointment;
+
+@Service
+public class AppointmentService {
+
+}
